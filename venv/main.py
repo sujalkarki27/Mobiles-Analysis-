314c4ecf3b955e2f23c21c28b326a208a04f509f
Sujal <sauravkarki51@gmail.com>
2025-04-04 21:34:29 +0530

    Change python version by venv

--- venv/main.py
+++ venv/main.py
@@ -1,6 +1,46 @@
 import pandas as pd 
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns 
 
+# Load the dataset
 df=pd.read_csv("/Users/sujalkarki/Desktop/Mobile Project /Mobiles.csv",encoding='latin1')
+print(df.head(5)) #display the first 5 rows
+print(df.columns)  #display the columns of the dataset
+df.info()   # Get the summary of the dataset
 
-print(df.head(5))
+# Change the value type of some columns from string to float
+numeric_columns = ['Mobile Weight', 'RAM', 'Front Camera', 'Back Camera', 'Battery Capacity', 
+                   'Launched Price (Pakistan)', 'Launched Price (India)', 'Launched Price (China)', 'Launched Price (USA)', 
+                   'Launched Price (Dubai)']
+
+for col in numeric_columns:
+    df[col] = df[col].str.replace(r'[^\d]', '', regex=True)
+    df[col] = df[col].replace('', 'NaN')
+    df[col] = df[col].astype(float)
+df.replace([np.inf, -np.inf], np.nan, inplace=True)
+
+expressions = ['inches','(main)','(internal)','(external)','(unfolded)', '()']
+for x in expressions:
+    df['Screen Size'] = df['Screen Size'].str.replace(x, '', regex=True)
+
+df['Screen Size'] = df['Screen Size'].apply(lambda x: x.split(' ')[0]).astype(float)
+
+# Add the units of measurement into the relevant columns
+df.rename(columns={'Mobile Weight':'Mobile Weight (grams)', 
+                   'Front Camera':'Front Camera (MP)',
+                   'RAM':'RAM (GB)',
+                   'Back Camera': 'BackCamera (MP)',
+                  'Battery Capacity':'Battery Capacity (mAh)',
+                  'Screen Size':'Screen Size (inches)'}, inplace=True)
+
+# Plot chart for the RAM Distribution
+sns.set_style("whitegrid")
+sns.set_palette('pastel')
+plt.figure(figsize=(15, 5))
+sns.countplot(x='RAM (GB)', data=df, edgecolor='black')
+plt.xlabel('RAM (GB)')
+plt.ylabel('Distribution')
+plt.title('RAM Distribution')
+plt.xticks(rotation=0);
+plt.show()
