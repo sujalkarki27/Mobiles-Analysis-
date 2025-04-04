459ff8ccd6a577b3234b208fe6c7b421518117bd
Sujal <sauravkarki51@gmail.com>
2025-04-04 19:57:03 +0530

    Fix error

--- venv/main.py
+++ venv/main.py
@@ -1,6 +1,6 @@
 import pandas as pd 
 import numpy as np
 
-df=pd.read_csv("/Users/sujalkarki/Desktop/Mobiles.csv",encoding='latin1')
+df=pd.read_csv("/Users/sujalkarki/Desktop/Mobile Project /Mobiles.csv",encoding='latin1')
 
 print(df.head(5))
